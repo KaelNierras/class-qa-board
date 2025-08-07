42b334ffd3cb54b509fd92226cce1c03bfd2268b
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2025-08-07 16:14:56 +0800

    removed uncessary items

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -89,14 +89,6 @@ export function NavUser({
                 <UserCircleIcon />
                 Account
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCardIcon />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <BellIcon />
-                Notifications
-              </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
