0b392ac444417a1c34617434fddb9fa8b1c35a38
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2025-08-07 13:46:42 +0800

    update button to auth button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import LoginButton from "@/components/LoginLogoutButton";
+import AuthButton from "@/components/AuthButton";
 import UserGreetText from "@/components/UserGreetText";
 import Image from "next/image";
 
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <UserGreetText />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <LoginButton />
+          <AuthButton />
         </div>
       </div>
 
