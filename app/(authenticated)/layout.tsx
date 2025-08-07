a7bc29e5ec7dc58a26ef848fd98d855059a3bc72
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2025-08-07 15:03:38 +0800

    Updated layout

---
+++ app/(authenticated)/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+
+interface AuthenticatedLayoutProps {
+    children: ReactNode
+}
+
+export default function AuthenticatedLayout({ children }: AuthenticatedLayoutProps) {
+    return (
+        <SidebarProvider>
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="p-5">
+                    {children}  
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
